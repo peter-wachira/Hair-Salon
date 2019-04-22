cddaab4298842e4b5f5b3bf0503d9f6b05e68f21
pwachira900@gmail.com <pwachira900@gmail.com>
2019-04-22 16:44:43 +0300

    cretae find me method

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,19 +1,20 @@
-import java.util.List;
 import org.sql2o.*;
-
+import java.util.ArrayList;
+import java.util.List;
 public class Client {
     private int id;
     private String name;
     private String gender;
     private String contact;
     private int stylist_id;
-
+    // client constuructor
     public Client(String name, String gender, String contact, int stylist_id) {
         this.name = name.toUpperCase();
         this.gender = gender;
         this.contact = contact;
         this.stylist_id = stylist_id;
     }
+    // client class getter methods
     public int getId(){
         return id;
     }
@@ -26,5 +27,40 @@ public class Client {
     public String getContact(){
         return contact;
     }
+    //return stylist Id
+    public Stylist getStylist_id() {
+        int id_check = stylist_id;
+        Stylist stylist;
+        if(id_check==0){
+            stylist = new Stylist("","","");
+        } else {
+            try(Connection con = DB.sql2o.open()) {
+                String sql = "SELECT * FROM stylists where id=:id";
+                stylist = con.createQuery(sql)
+                        .addParameter("id", id)
+                        .executeAndFetchFirst(Stylist.class); }
+        }
+        return stylist;
+      }
+      //return a Client object.
+      public static Client find(int id) {
+          try(Connection con = DB.sql2o.open()) {
+              String sql = "SELECT * FROM clients where id=:id";
+              Client client = con.createQuery(sql)
+                      .addParameter("id", id)
+                      .executeAndFetchFirst(Client.class);
+              return client;
+          }
+      }
+
+      //updating Client.all() method's SQL query to include Sylist Id.
+      public static List<Client> all() {
+          String sql = "SELECT id, name, gender, contact, stylist_id FROM clients ORDER BY name";
+          try(Connection con = DB.sql2o.open()) {
+              return con.createQuery(sql).executeAndFetch(Client.class);
+          }
+      }
+      //
+
 
 }
