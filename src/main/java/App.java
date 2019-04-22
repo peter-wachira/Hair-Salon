84cef548c8de6b24a2f1d50b33bee38cfa81fd0a
pwachira900@gmail.com <pwachira900@gmail.com>
2019-04-22 19:06:48 +0300

    created process builder process

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,7 +6,59 @@ import static spark.Spark.*;
 
 
 public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+        String layout = "templates/layout.vtl";
+
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+
+        setPort(port);
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 
 }
