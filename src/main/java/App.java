e9e250d5c3b9b6da8f9ebb48c9a8064126441a1c
pwachira900@gmail.com <pwachira900@gmail.com>
2019-04-22 22:04:52 +0300

    add client form editor

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,16 +6,30 @@ import static spark.Spark.*;
 
 
 public class App {
+
     public static void main(String[] args) {
         staticFileLocation("/public");
         String layout = "templates/layout.vtl";
 
-        //homepage path
+
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+
+        port(port);
+
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
             model.put("template", "templates/index.vtl");
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
+
+
+
 
 
 
