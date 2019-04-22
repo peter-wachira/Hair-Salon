af16377b3c7065adbdab2632889c0bdc26fa476d
pwachira900@gmail.com <pwachira900@gmail.com>
2019-04-22 16:57:09 +0300

    created getId method

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -1,14 +1,19 @@
 import java.util.List;
 import org.sql2o.*;
 
-public class Stylist{
+public class Stylist {
     private int id;
     private String name;
     private String gender;
     private String contact;
 
-    public Stylist(String name, String gender, String contact){
+    public Stylist(String name, String gender, String contact) {
         this.name = name.toUpperCase();
         this.gender = gender;
         this.contact = contact;
     }
+    public int getId(){
+        return id;
+    }
+
+}
